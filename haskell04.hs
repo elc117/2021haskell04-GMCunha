dc6d48d1a9546dd5f7939366254cb13d0d9958a6
GMCunha <83093980+GMCunha@users.noreply.github.com>
2021-06-02 10:23:45 -0300

    1 a 3 feitas

--- haskell04.hs
+++ haskell04.hs
@@ -1,2 +1,17 @@
 -- Prática 04 de Haskell
--- Nome: 
+-- Nome: Guilherme Medeiros da Cunha
+
+faixaIdoso :: Int -> String
+faixaIdoso i
+    | i < 60 = "ND"
+    | i < 65 = "IDO64"
+    | i < 70 = "IDO69"
+    | i < 75 = "IDO74"
+    | i < 80 = "IDO79"
+    | otherwise = "IDO80"
+
+classifIdosos :: [(String,Int)] -> [(String,Int,String)]
+classifIdosos l = [(x, y, faixaIdoso y) | (x, y) <- l]
+
+classifIdosos' :: [(String,Int)] -> [(String,Int,String)]
+classifIdosos' l = map (\ (x, y) -> (x, y, faixaIdoso y)) l
